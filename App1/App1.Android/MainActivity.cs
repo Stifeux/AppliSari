3ada09860f46ddbd38a04a0b94b1e47abd1425e0
Stifeux <stifeu@hotmail.fr>
2019-01-16 11:25:35 +0100

    Ajoutez des fichiers projet.

---
+++ App1/App1.Android/MainActivity.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace App1.Droid
+{
+    [Activity(Label = "App1", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+        TabLayoutResource = Resource.Layout.Tabbar;
+        ToolbarResource = Resource.Layout.Toolbar;
+    
+        base.OnCreate(savedInstanceState);
+        global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+        LoadApplication(new App());
+        }
+    }
+
+    public class GmailAPI : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/gmail-dotnet-quickstart.json
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string ApplicationName = "Gmail API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+            Console.WriteLine("Pouet");
+            // List labels.
+            IList<Label> labels = request.Execute().Labels;
+            Console.WriteLine("Labels:");
+            if (labels != null && labels.Count > 0)
+            {
+                foreach (var labelItem in labels)
+                {
+                    Console.WriteLine("{0}", labelItem.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No labels found.");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using SignaturePad.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    class PickerDemoPage : ContentPage
+    {
+        // Dictionary to get Color from color name.
+        Dictionary<string, string> delaiToValue = new Dictionary<string, string>
+        {
+            { "4h ou moins", "<=4" }, { "Plus de 4h", ">4" },
+            { "1 jour ou plus", "1j" }
+        };
+
+        public PickerDemoPage()
+        {
+            Picker picker = new Picker
+            {
+                Title = "Delai",
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            foreach (string delai in delaiToValue.Keys)
+            {
+                picker.Items.Add(delai);
+            }
+
+            picker.SelectedIndexChanged += (sender, args) =>
+            {
+                System.Diagnostics.Debug.WriteLine(picker.SelectedItem);
+            };
+
+            SignaturePadView signatureView = new SignaturePadView()
+            {
+                StrokeWidth = 3f,
+                StrokeColor = Color.Black,
+                BackgroundColor = Color.White
+            };
+
+            // Build the page.
+
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    picker,
+                    signatureView
+                }
+            };
+
+        }
+    }
+
+    class gridTest : ContentPage
+    {
+        public gridTest()
+        {
+            var grid = new Grid();
+
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            var topLeft = new Label { Text = "Top Left" };
+            var topRight = new Label { Text = "Top Right" };
+            var bottomLeft = new Label { Text = "Bottom Left" };
+            var bottomRight = new Label { Text = "Bottom Right" };
+
+            grid.Children.Add(topLeft, 0, 0);
+            grid.Children.Add(topRight, 1, 0);
+            grid.Children.Add(bottomLeft, 0, 1);
+            grid.Children.Add(bottomRight, 1, 1);
+        }
+    }
+}
